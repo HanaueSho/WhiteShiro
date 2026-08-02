e456b14340134c111e0d60decaa174180a1eefd2
HanaueSho <HanaueSho@users.noreply.github.com>
2026-08-02 21:26:28 +0900

    フラッグ、ゴルドーの追加
    フラッグとゴルドーのモデルを追加。
    Block_Goal, Block_NoTouch, Block_NoTouch_Destroyをプレファブ化しました。
    SceneManager_LevelSceneにゴールと死亡を通知するようにしました。

--- Assets/Hanaue/Scripts/ReactionComponent/Reactions/Reaction_Goal.cs
+++ Assets/Hanaue/Scripts/ReactionComponent/Reactions/Reaction_Goal.cs
@@ -8,9 +8,23 @@ using UnityEngine;
 
 public class Reaction_Goal : ReactionComponent
 {
+    [SerializeField]
+    private SceneManager_LevelScene _smLevel;
+
+    private void Start()
+    {
+        _smLevel = GameObject.FindAnyObjectByType<SceneManager_LevelScene>();
+        if (_smLevel == null )
+        {
+            Debug.LogWarning("[Warning] Not Find SceneManager_LevelScene!!!");
+        }
+    }
+
     public override bool Enter(Block influencer, CommandComponent command)
     {
         // ここでクリアを通知
+        _smLevel?.OnGoal();
+
         return base.Enter(influencer, command);
     }
 
--- Assets/Hanaue/Scripts/ReactionComponent/Reactions/Reaction_NoTouch.cs
+++ Assets/Hanaue/Scripts/ReactionComponent/Reactions/Reaction_NoTouch.cs
@@ -7,9 +7,22 @@ using UnityEngine;
 
 public class Reaction_NoTouch : ReactionComponent
 {
+    private SceneManager_LevelScene _smLevel;
+
+    private void Start()
+    {
+        _smLevel = GameObject.FindAnyObjectByType<SceneManager_LevelScene>();
+        if (_smLevel == null)
+        {
+            Debug.LogWarning("[Warning] Not Find SceneManager_LevelScene!!!");
+        }
+    }
+
     public override bool Enter(Block influencer, CommandComponent command)
     {
         // ここで死亡を通知
+        _smLevel?.OnGameOver();
+
         return base.Enter(influencer, command);
     }
 }
