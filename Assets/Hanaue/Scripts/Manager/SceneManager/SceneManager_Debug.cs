ceaa82cb582e47b58a1f7855e0f1c6a7710d43cd
HanaueSho <HanaueSho@users.noreply.github.com>
2026-08-03 02:14:02 +0900

    デバッグシーンを整えました
    デバッグシーンに入ると自動的にタイトルシーンへ遷移するようにしました。
    残りはBGMでβ版完成！

---
+++ Assets/Hanaue/Scripts/Manager/SceneManager/SceneManager_Debug.cs
@@ -0,0 +1,16 @@
+﻿/*
+    SceneManager_Debug.cs
+    20260803  hanaue sho
+    デバッグシーンを管理するマネージャー
+*/
+using UnityEngine;
+
+public class SceneManager_Debug : SceneManager_Base
+{
+    void Start()
+    {
+        // 速攻で Title シーンへ遷移する
+        base.Exit(1);
+    }
+
+}
